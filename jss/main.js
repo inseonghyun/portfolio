df3566c4e0662a8af72468ad45ded2e5af9276e9
ChaeChaelini <bona0901@naver.com>
2023-11-02 18:15:39 +0900

    13

--- jss/main.js
+++ jss/main.js
@@ -35,3 +35,18 @@ $(function () {
         });
     };
 }); // jQuery
+
+const pics = $(".pic");
+const lightbox = $("#lightbox");
+const lightImg = $("#lightImage");
+pics.on('click', function () {
+    const bigLocation = $(this).attr("data-src");
+    lightImg.load(bigLocation);
+    lightbox.css('display', 'block');
+    $('html').addClass('all_scrollFixed');
+});
+
+lightbox.on('click', function () {
+    lightbox.css('display', 'none');
+    $('html').removeClass('all_scrollFixed');
+})
